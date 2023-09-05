3d44b5a3befbef1ea3e051c38ca508ae00838c5b
saranmac <matoda4113>
2023-09-05 17:22:10 +0900

    d

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+///MainPage
+///담당자 : ---
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("MainPage"),
+    );
+  }
+}
