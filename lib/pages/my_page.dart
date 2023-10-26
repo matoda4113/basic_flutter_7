0dbf83fe6d6e15fa551c19c0b44052975a65bf13
saranmac <matoda4113>
2023-10-26 19:45:07 +0900

    d

--- lib/pages/my_page.dart
+++ lib/pages/my_page.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:basic_flutter_7/controller/user_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 ///MyPage
 ///담당자 : ---
@@ -38,18 +41,34 @@ class _MyPageState extends State<MyPage> {
                       color: Colors.blue,
                       shape: BoxShape.circle
                   ),
-                  child: Image.network(
-                    controller.myInfo?.imagePath??"",fit: BoxFit.cover,
-                    errorBuilder: (context, error, stackTrace) {
-                      return Icon(
-                        Icons.face
-                      ); // 이미지 로드 오류 시 대체 이미지
-                    },
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+                    child: Image.network(
+                      controller.myInfo?.imagePath??"",fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                        return Icon(
+                          Icons.face
+                        ); // 이미지 로드 오류 시 대체 이미지
+                      },
+                    ),
                   ),
                 ),
                 SizedBox(height: 50,),
-                ElevatedButton(onPressed: (){
+                ElevatedButton(onPressed: () async{
+                  //1. 이미지 피커로 사진받기
+                  XFile? xFile = await ImagePicker().pickImage(source: ImageSource.gallery);
 
+                  //2. XFile -> File로 변환
+                  if (xFile != null) {
+                    File file = File(xFile.path);
+
+                    //3. 컨트롤러 메소드 호출
+                    try{
+                      controller.editProfileImage(file);
+                    }catch(e){
+                      print("에러낫슈");
+                    }
+                  }
                 }, child: Text("사진올리기")),
                 SizedBox(height: 50,),
                 Container(
